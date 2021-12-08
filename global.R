82430a0396c102b30a8f450f1234f05a6a126d78
tomasbird <tomasbird@gmail.com>
2021-12-07 21:51:37 -0800

    packageloader

--- global.R
+++ global.R
@@ -1,25 +1,41 @@
-library(dplyr)
-library(shiny)
-library(leaflet)
-library(RColorBrewer)
-library(scales)
-library(lattice)
-library(sp)
-library(rgdal)
-library(ggplot2)
-library(shinycssloaders)
-library(cvms)
-library(car)
-library(Rcpp)
-library(Metrics)
-library(merTools)
-library(leaps)
-library(sf)
-library(tidyr)
-library(groupdata2)
-library(pROC)
-library(DT)
+#library(dplyr)
+#library(leaflet)
+##library(shiny)
+#library(RColorBrewer)
+#library(lattice)
+#library(scales)
+#library(sp)
+#library(rgdal)
+#library(shinycssloaders)
+#library(ggplot2)
+#library(cvms)
+#library(car)
+#library(Rcpp)
+#library(Metrics)
+#library(merTools)
+#library(leaps)
+#library(sf)
+#library(tidyr)
+#library(groupdata2)
+#library(pROC)
+#library(DT)
+
 options(shiny.maxRequestSize=200*1024^2)
+
+packages=c("DT", "pROC", "groupdata2", "tidyr", "sf", "leaps", "merTools", "Metrics", "Rcpp", "car", 
+           "cvms", "shinycssloaders", "ggplot2", "rgdal", "sp", "lattice", "scales", "RColorBrewer", 
+           "leaflet", "shiny", "dplyr")
+
+## Now load or install&load all
+package.check <- lapply(
+  packages,
+  FUN = function(x) {
+    if (!require(x, character.only = TRUE)) {
+      install.packages(x, dependencies = TRUE)
+      library(x, character.only = TRUE)
+    }
+  }
+)
 
 
 tags$style(type="text/css", "
